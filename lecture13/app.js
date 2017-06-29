fc366afa7f867b12b8f82d39e336ff7333e26ce6
doudouplo <doudouplo@free.fr>
2017-06-29 18:41:12 +0200

    lect13

--- lecture13/app.js
+++ lecture13/app.js
@@ -1,13 +1,18 @@
 (function(){
   var app = angular.module("lect13", []);
 
-  app.controller("nomDuController", function($scope,$filter){
+  app.controller("nomDuController", function($scope, $filter){
     $scope.uneVariable = "une variable";
 
     $scope.uneFonction = function(){
       return "ceci est un retour de fonction";
     };
 
+    $scope.montant = $filter("uppercase") ("texte majuscule");
 
+    var texte = "un texte en majuscule";
+    var filtre = $filter("uppercase")(texte);
+    texte = "un autre texte"
+    $scope.texteMaj = filtre;
   });
 })();
