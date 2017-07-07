991a9b3d6ed6a3d357aea384befcdceb1c6f6264
doudou <doudouplo@free.fr>
2017-07-07 15:04:40 +0200

    e

--- eventArgument/script.js
+++ eventArgument/script.js
@@ -1,3 +1,4 @@
+//https://developer.mozilla.org/en-US/docs/Web/API/Event
 document.addEventListener("DOMContentLoaded",
   function(event){
 
@@ -10,9 +11,21 @@ document.addEventListener("DOMContentLoaded",
         //change texte bouton
         this.textContent = "c'est dit";
       }
-
       //Unobstrusive event binding
       document.querySelector("button")
         .addEventListener("click", cliquer);
 
+      //affiche mouse position si ctrl
+      var mouseMove = function(event){
+        if (event.ctrlKey){
+          document.querySelector("#pos").textContent =
+            "x: " + event.clientX + ", y: " + event.clientY;
+        }
+      };
+      document.addEventListener("mousemove", mouseMove);
+
+      var mouseBodyMove = function(event){
+        console.log("mouseBodyMove");
+      };
+      document.querySelectorAll("body")[0].addEventListener("mousemove", mouseBodyMove);
   });
